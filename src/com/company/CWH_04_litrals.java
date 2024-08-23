04f41ffd41888a7453de7d73b5f97aa3b23e60f0
MOHAMMAD SHADAB ABEDIN <shadababe04@gmail.com>
2024-08-23 21:56:57 +0530

    litrals tutorial

---
+++ src/com/company/CWH_04_litrals.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class CWH_04_litrals {
+    public static void main(String[] args) {
+        System.out.print("The sum of these number: ");
+        int num1 = 54;
+        int num2 = 78;
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
