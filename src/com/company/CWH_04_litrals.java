38560120fdcd4e0e9456a33e94e91bc9ea079ada
MOHAMMAD SHADAB ABEDIN <shadababe04@gmail.com>
2024-08-23 22:09:49 +0530

    litrals tutorial cont.

---
+++ src/com/company/CWH_04_litrals.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class CWH_04_litrals {
+    public static void main(String[] args) {
+        byte age = 32;
+        int age2 = 35;
+        short age3 = 23;
+        long age4 = 599_999_394;
+        char ch = 'S';
+        float f1 = 5.6f;
+        double d1 = 4.55;
+        boolean a = true;
+        System.out.println(age4);
+    }
+}
